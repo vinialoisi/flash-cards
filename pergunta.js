1d87879f90c51bf55ff64213218871dd1f0c3ac5
vinialoisi <viniciusaloisi@gmail.com>
2024-11-28 13:51:28 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI(RAI)'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são monções?',
+    'Monções são um fenômeno atmosférico caracterizado pela alteração sazonal na direção dos ventos que sopram em determinada localidade.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é relevo?',
+    'conjunto de saliências e reentrâncias de uma superfície, esp. da superfície da Terra.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
